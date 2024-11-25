02558f943e1c4fe1adfca8d64843dbafc7bf2343
gabasaura <gabriela.garin@gmail.com>
2024-11-24 22:46:01 -0300

    update

--- app/Livewire/Dashboard/Category/Save.php
+++ app/Livewire/Dashboard/Category/Save.php
@@ -7,27 +7,52 @@ use Livewire\Component;
 
 class Save extends Component
 {
+    public $category;
     public $title;
     public $text;
+
     protected $rules = [
         'title' => 'required|min:2|max:255',
-        'text' => 'nullable'
+        'text' => 'required|min:2|max:255',
     ];
+
+     function mount(?int $id = null)
+    {
+        
+
+        if ($id != null) {
+            $this->category = Category::findOrFail($id);
+            $this->title = $this->category->title;
+            $this->text = $this->category->text;
+        };
+    }
 
     public function render()
     {
         return view('livewire.dashboard.category.save');
     }
 
-    function submit() 
+     function submit()
     {
         $this->validate();
-        Category::create(
-        [
-        'title' => $this->title,
-        'text' => $this->text,
-        'slug' => str($this->title)->slug(),
-        ]
-        );
+        if ($this->category) {
+            $this->category->update([
+                'title' => $this->title,
+                'text' => $this->text,
+            ]);
+
+        } else {
+
+            Category::create(
+                [
+                    'title' => $this->title,
+                    'text' => $this->text,
+                    'slug' => str($this->title)->slug(),
+                ]
+            );
+        }
     }
+    
+    
+    
 }
--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,16 +1,22 @@
 <div>
     <form wire:submit.prevent='submit'>
-        @error('title')
-            {{ $message }}
-        @enderror
+        {{ $title }}
 
-        <label for= "">TITULO</label>
+        <label for="">TITULO</label>
         <input type="text" wire:model='title'>
-        
+
         <label for="">TEXTO</label>
         <input type="text" wire:model='text'>
 
-
+        @if ($errors->any())
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+        @endif
 
         <button type="submit" wire:keydown.enter='submit'>ENVIAR</button>
     </form>
