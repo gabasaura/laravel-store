1e3f45e14fc5899338e70cf3d59be318748778e3
gabasaura <gabriela.garin@gmail.com>
2024-11-24 21:43:25 -0300

    error

--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,11 +1,16 @@
 <div>
     <form wire:submit.prevent='submit'>
-        {{ $title }}
+        @error('title')
+            {{ $message }}
+        @enderror
+
         <label for= "">TITULO</label>
         <input type="text" wire:model='title'>
-
+        
         <label for="">TEXTO</label>
         <input type="text" wire:model='text'>
+
+
 
         <button type="submit" wire:keydown.enter='submit'>ENVIAR</button>
     </form>
